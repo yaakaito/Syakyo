49ef9df154223eed7bd8e0d4255c7108eeca211e
KAZUMA Ukyo <yaakaito@gmail.com>
2013-04-01 21:13:58 +0900

    Add todo view

---
+++ backbone.todomvc/js/views/todos.js
@@ -0,0 +1,26 @@
+/* global Backbone */
+
+var app = app || {};
+
+$(function() {
+  'use strict';
+
+  app.TodoView = Backbone.View.extend({
+    tagName : 'li',
+    template : _.temlate($('#item-template').html()),
+
+    initialize : function(){
+      this.listenTo(this.model, 'change', this.render);
+    },
+
+    render : function(){
+      this.$el.html(this.template(this.model.toJSON()));
+      this.$el.toggleClass('completed', this.model.get('completed'));
+      this.$el.toggleClass('hidden', this.isHidden());
+      this.$input = this.$('.edit');
+      return this;
+    },
+
+    
+  })
+});
